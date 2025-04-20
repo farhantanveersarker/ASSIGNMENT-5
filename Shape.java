5ce644d489e61c16b3061dfeb82b7abc13979cd0
farhantanveersarker <157597608+farhantanveersarker@users.noreply.github.com>
2025-04-20 22:52:11 +0530

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,11 @@
+abstract class Shape {
+  abstract void calculateShape();
+
+  abstract void calculatePerimeter();
+
+  void showShape(String shape) {
+    System.out.println("Selected shape: " + shape);
+    calculateShape();
+    calculatePerimeter();
+  }
+}
